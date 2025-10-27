4ac9f6b5a96848629cf3f851bb52fd0a386c4f90
Brandon <32753167+btoms20@users.noreply.github.com>
2025-10-27 14:31:44 -0700

    removed platforms

--- Package.swift
+++ Package.swift
@@ -17,10 +17,6 @@ import PackageDescription
 
 let package = Package(
     name: "swift-multihash",
-    platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
